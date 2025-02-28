e05c091e464e4b16a000102b56188d11d3603cee
Erick Fernando Carvalho Sanchez <carvalhosanchezerickfernando@gmail.com>
2025-02-28 08:12:36 -0300

    basic configuration with react query

--- src/App.jsx
+++ src/App.jsx
@@ -1,74 +1,81 @@
-import { useState, } from 'react'
-import ButtonGroup from 'react-bootstrap/ButtonGroup'
+import { setNotification, clearNotification, } from './contexts/notifications'
+import { useContext, } from 'react'
+import { useMutation, useQuery, useQueryClient, } from '@tanstack/react-query'
+import AnecdoteForm from './components/AnecdoteForm'
+import anecdotesServices from './services/anecdotes'
+import Button from 'react-bootstrap/Button'
 import Card from 'react-bootstrap/Card'
+import Navbar from 'react-bootstrap/Navbar'
+import Notification from './components/Notification'
+import notificationContext from './contexts/notifications'
 import Stack from 'react-bootstrap/Stack'
-import StyledButton from 'react-bootstrap/Button'
 
-const Button = ({ text, onClick, variant, style, },) => (
-    <>
-        <StyledButton variant={variant} style={style} onClick={onClick}>{text}</StyledButton>
-    </>
-)
 
 const App = () => {
-    const anecdotes = [
-        'If it hurts, do it more often.',
-        'Adding manpower to a late software project makes it later!',
-        'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-        'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-        'Premature optimization is the root of all evil.',
-        'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-        'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
-        'The only way to go fast, is to go well.',
-    ]
 
-    const [selected, setSelected,] = useState(0,)
-    const [votes, setVotes,] = useState(Array(anecdotes.length,).fill(0,),)
-    const [maxVoteIndex, setMaxVoteIndex,] = useState(0,)
+    const [notification, notificationDispatch,] = useContext(notificationContext,)
 
-    const handleNextAnecdote = () => {
-        setSelected(Math.floor(Math.random() * anecdotes.length,),)
+    const result = useQuery({
+        queryKey: ['anecdotes',],
+        queryFn: anecdotesServices.fetchAll,
+        retry: 0,
+        refetchOnWindowFocus: false,
+    },)
+
+    const anecdotes = result.data
+
+    const queryClient = useQueryClient()
+    const anecdotesMutation = useMutation({
+        mutationFn: anecdotesServices.updateAnecdote,
+        onSuccess: (res,) => {
+            queryClient.setQueryData(['anecdotes',], anecdotes.map((e,) => (e.id === res.id ? { ...e, votes: e.votes + 1, } : e),),)
+            notificationDispatch(setNotification(`${res.content} was voted`,),)
+            setTimeout(() => {
+                notificationDispatch(clearNotification(),)
+            }, 5000,)
+        },
+        onError: (err,) => {
+            notificationDispatch(setNotification(`${err.content} wasn't voted`,),)
+            setTimeout(() => {
+                notificationDispatch(clearNotification(),)
+            }, 5000,)
+        },
+    },)
+
+    const handleVote = (anecdote,) => {
+        anecdotesMutation.mutate({ ...anecdote, votes: anecdote.votes + 1, },)
     }
 
-    const handleVote = () => {
-        let newVotes = [...votes,]
-        newVotes[selected] += 1
-        if (newVotes[selected] > newVotes[maxVoteIndex]) setMaxVoteIndex(selected,)
-        setVotes(newVotes,)
+    if (result.isLoading) {
+        return (
+            <div>loading data ...</div>
+        )
+    } else if (result.error) {
+        return (
+            <div>anecdote service not available due to problems in the server</div>
+        )
     }
 
     return (
         <Stack gap={3} style={{ marginTop: 30, marginBottom: 30, }}>
-            <Card>
-                <Card.Body>
-                    <Card.Title >
-                        <h2 style={{ textAlign: 'center', }}>Anecdote of the day</h2>
-                    </Card.Title>
-                    <Card.Text style={{ textAlign: 'center', }}>
-                        {anecdotes[selected]}
-                        <br />
-                        has {votes[selected]} votes
-                    </Card.Text>
-                    <Stack direction='horizontal' style={{ justifyContent: 'center', }}>
-                        <ButtonGroup size='lg' style={{ justifyContent: 'center', width: '90%', maxWidth: 500, }}>
-                            <Button text='Vote' onClick={handleVote} variant='success' style={{ width: '50%', }} />
-                            <Button text='Next anecdote' onClick={handleNextAnecdote} variant='secondary' style={{ width: '50%', }} />
-                        </ButtonGroup>
-                    </Stack>
-                </Card.Body>
-            </Card >
-            <Card>
-                <Card.Body>
-                    <Card.Title>
-                        <h2 style={{ textAlign: 'center', }}>Anecdote with most votes</h2>
-                    </Card.Title>
-                    <Card.Text style={{ textAlign: 'center', }}>
-                        {anecdotes[maxVoteIndex]}
-                        <br />
-                        has {votes[maxVoteIndex]} votes
-                    </Card.Text>
-                </Card.Body>
-            </Card>
+            <Navbar>
+                <Navbar.Brand>Mini Anecdotes App</Navbar.Brand>
+            </Navbar>
+            <Notification message={notification} />
+            <AnecdoteForm />
+            {anecdotes.map(anecdote =>
+                <Card key={anecdote.id}>
+                    <Card.Body>
+                        <Stack direction='horizontal' style={{ justifyContent: 'space-between', }}>
+                            {anecdote.content}
+                            <Stack gap={2} direction='horizontal' style={{ justifyContent: 'end', minWidth: 'max-content', }}>
+                                Votes: {anecdote.votes}
+                                <Button variant='primary' onClick={() => handleVote(anecdote,)}>vote</Button>
+                            </Stack>
+                        </Stack>
+                    </Card.Body>
+                </Card>,
+            )}
         </Stack>
     )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,14 @@
-import { createRoot, } from 'react-dom/client'
-import App from './App.jsx'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import { QueryClient, QueryClientProvider, } from '@tanstack/react-query'
+import { NotificationContextProvider, } from './contexts/notifications'
 
-createRoot(document.getElementById('root',),).render(<App />,)
+const queryClient = new QueryClient()
+
+ReactDOM.createRoot(document.getElementById('root',),).render(
+    <NotificationContextProvider >
+        <QueryClientProvider client={queryClient}>
+            <App />
+        </QueryClientProvider>
+    </NotificationContextProvider>,
+)
